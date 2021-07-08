eb50330f2a4c74fcc5f457d6adc377be36e82e57
Nguyen-Van-Manh <manh117bg@gmail.com>
2021-07-08 09:43:56 +0700

    add cross env, config ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'lingo-server',
       script: './dist/main.js',
       cwd: __dirname,
-      instances: 2,
+      instances: 1,
       autorestart: true,
       exec_mode: 'cluster',
       env: {
@@ -12,34 +12,4 @@ module.exports = {
       },
     },
   ],
-
-  deploy: {
-    production: {
-      host: '192.168.31.111',
-      user: 'deploy',
-      ssh_options: [
-        'ForwardAgent=yes',
-      ],
-      ref: 'origin/master',
-      repo: 'git@github.com:Nguyen-Van-Manh/lingo-server.git',
-      path: '‪C:\Dev\lingo-server',
-      'post-deploy': '‪C:\Dev\lingo-server && NODE_ENV=production yarn --production=false;yarn build;pm2 startOrReload ecosystem.config.js',
-      env: {
-        NODE_ENV: 'production',
-      },
-    },
-
-    staging: {
-      user: 'deploy',
-      host: '192.168.31.111',
-      ref: 'origin/master',
-      repo: 'git@github.com:Nguyen-Van-Manh/lingo-server.git',
-      path: '/path/to/project',
-      ssh_options: ['PasswordAuthentication=no', 'StrictHostKeyChecking=no'],
-      'post-deploy': 'cd /path/to/project && yarn --production=false;yarn build;pm2 startOrReload ecosystem.config.js',
-      env: {
-        NODE_ENV: 'production',
-      },
-    },
-  },
 };
