dd8428b85c311ea7ddb448ec4a414c7192b04557
Nguyen-Van-Manh <manh117bg@gmail.com>
2021-07-15 22:50:48 +0700

    updateg

--- src/app-cluster.service.ts
+++ src/app-cluster.service.ts
@@ -13,7 +13,7 @@ export class AppClusterService {
       for (let i = 0; i < numCPUs; i++) {
         cluster.fork();
       }
-      cluster.on('exit', (worker, code, signal) => {
+      cluster.on('exit', (worker) => {
         console.log(`Worker ${worker.process.pid} died. Restarting`);
         cluster.fork();
       });
