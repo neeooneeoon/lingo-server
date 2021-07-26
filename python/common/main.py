8c288789dc3b17cfb198e6037bdfb1a88be27f13
Nguyen-Van-Manh <manh117bg@gmail.com>
2021-07-26 21:42:59 +0700

    add lowest common

---
+++ python/common/main.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+
+class QuestionTypeCode(Enum):
+    W3 = 'P1-I1W2-C'
+    W6 = 'P1-W1-C'
+    W11 = 'P1-W1-W'
+    W7 = 'P1I1-W1-W'
+    W2 = 'P1W1-I1-C'
+    W4 = 'W1-I1P1-C'
+    W12 = 'P1W1-P1-V'
+    W9 = 'W1-W2-M'
+    W8 = 'W1-W2-W'
+    W13 = 'W2-W1-C'
+    W14 = 'W2-W1-W'
+    W15 = 'W1-W2-C'
+    S12 = 'S1-S2-R'
+    S10 = 'S2-S1-C'
+    S1 = 'A1-S1-R'
+    S2 = 'S2-S1-R'
+    S14 = 'A1-S1-W'
+    S17 = 'S1-S1-R'
+    S7 = 'A1-W1S1-C'
+    S15 = 'A1-S1-W'
+    S16 = 'A1S1-S2-W'
+    S4 = 'A1S1-A1-V'
+    S18 = 'S2-S1-W'
--- python/stories/main.py
+++ python/stories/main.py
@@ -3,6 +3,7 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import wordnet as wn
 from drive import database
+from python.common.main import QuestionTypeCode
 
 
 def is_noun(tag: str) -> bool:
@@ -33,10 +34,76 @@ def penn_to_wn(tag):
     return wn.NOUN
 
 
+def in_unit_words(word_content: str, unit_words: list):
+    for word in unit_words:
+        if word["content"].strip().lower() == word_content:
+            return word
+    return None
+
+
+def in_choices(choices: list, item: dict) -> bool:
+    content = item["content"]
+    meaning = item["meaning"]
+    if not content or not meaning:
+        return False
+    for choice in choices:
+        if choice["content"] == content or choice["meaning"] == meaning:
+            return True
+    return False
+
+
+def lowest_common_hypernyms(words: list, word: dict, choices: list):
+    clone_choices = list(choices)
+    marker_index = len(clone_choices)
+    word_synsets = wn.synsets(word["content"].lower().strip().replace(" ", "_"))
+    if word_synsets and words:
+        for i in range(len(word_synsets)):
+            for w in list(words):
+                content = w["content"].lower().strip()
+                meaning = w["meaning"].lower().strip()
+                id = w["_id"]
+                choice_item = {"content": content, "meaning": meaning}
+                if not in_choices(clone_choices, choice_item):
+                    w_synsets = wn.synsets(content.replace(" ", "_"))
+                    if w_synsets:
+                        for ss in w_synsets:
+                            rate = word_synsets[i].path_similarity(ss, simulate_root=False)
+                            item = {
+                                "content": content,
+                                "_id": id,
+                                "rate": rate,
+                                "meaning": meaning
+                            }
+                            if rate is None:
+                                words.remove(w)
+                                break
+                            elif 0.2 <= rate < 1.0:
+                                clone_choices.append(item)
+                                words.remove(w)
+                                break
+                else:
+                    words.remove(w)
+    latest = sorted(clone_choices, key=lambda i: i["rate"], reverse=True)
+    return latest[marker_index:]
+
+
 async def stories_in_unit(book_id: str, unit_id: str) -> list:
     cursor = database["stories"].find({
         "bookId": book_id,
         "unitId": unit_id,
+    })
+    return [doc for doc in cursor]
+
+
+async def books() -> list:
+    cursor = database["books"].find()
+    return [doc for doc in cursor]
+
+
+async def words_in_unit(book_nId: int, unit_nId: int) -> list:
+    cursor = database["words"].find({
+        "bookNId": book_nId,
+        "unitNId": unit_nId,
     })
     return [doc for doc in cursor]
 
