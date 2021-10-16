cff45f3dbfd17544008296e6e2b03db34488697c
manhhnv <manh117bg@gmail.com>
2021-10-16 11:48:56 +0700

    fix redis-port

--- packages/extensions/notifications/queue.js
+++ packages/extensions/notifications/queue.js
@@ -4,7 +4,7 @@ import { get } from '../configs/index.js';
 const config = {
   redis: {
     host: get('REDIS_HOST'),
-    port: Number(get('REDIS_PORT')),
+    port: Number(get('REDIS_PORT')) ? Number(get('REDIS_PORT')) : 6379,
   },
 };
 
